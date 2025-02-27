6b034d6fb482ccb89fd8d3630adc7f48453f8153
Obieda11 <obiedamansour@gmail.com>
2025-02-27 23:06:33 +0000

    Feat: added seating capacity

--- car-rental-service.py
+++ car-rental-service.py
@@ -18,6 +18,13 @@ class vehicle:
     def set_rental_price_per_days(self, new_price):
         self.get_rental_price_per_days = new_price
     
+
+
+
+class car(vehicle):
+    def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
+        super().__init__(brand, model, year, rental_price_per_day)
+        self.seating_capacity = seating_capacity
     
 
 
